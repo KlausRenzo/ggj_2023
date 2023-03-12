bb667f64f4417dc74bea71bf5ae9522a088dbd9f
Cinfa <cinfa78@gmail.com>
2023-03-12 19:23:13 +0100

    glasses laser audio and vfx

--- src/Assets/Scripts/Aesthetic/GlassesWeapon.cs
+++ src/Assets/Scripts/Aesthetic/GlassesWeapon.cs
@@ -12,16 +12,30 @@ public class GlassesWeapon : MonoBehaviour {
 	[SerializeField] private GameObject laser;
 	[SerializeField] private float pushingForce;
 	[SerializeField] private float cooldown;
+	[SerializeField] private ParticleSystem particleSystem;
+	[SerializeField] private AudioClip chargingClip;
+	[SerializeField] private AudioClip firingClip;
+	private AudioSource audioSource;
 	private bool firing;
 
 	private void Awake() {
 		currentTimer = 0;
 		laser.SetActive(false);
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if ((playerLayer == (playerLayer | (1 << other.gameObject.layer)))) {
+			particleSystem.Play();
+			if (!audioSource.isPlaying)
+				audioSource.PlayOneShot(chargingClip);
+		}
 	}
 
 	private void OnTriggerExit(Collider other) {
 		if ((playerLayer == (playerLayer | (1 << other.gameObject.layer)))) {
 			currentTimer = 0;
+			particleSystem.Stop();
 		}
 	}
 
@@ -32,6 +46,7 @@ public class GlassesWeapon : MonoBehaviour {
 	}
 
 	private void OnCollisionEnter(Collision collision) {
+		audioSource.PlayOneShot(firingClip);
 		var bullet = collision.gameObject.GetComponent<Bullet>();
 		if (bullet != null) {
 			health += bullet.GetDamage();
